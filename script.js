f1d4655b64446a8ccf6c2d3cbfa7a9f666444747
petero00 <145260611+petero00@users.noreply.github.com>
2024-01-05 15:52:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const pop = new Audio('pop.mp3');
 function aaaa(e) {
     console.log(e.pageX + ',' + e.pageY);
     let A = document.createElement('div');
@@ -5,4 +6,5 @@ function aaaa(e) {
     A.style.left = (e.pageX - 25) + 'px'
     A.style.top = (e.pageY - 25) + 'px'
     document.getElementById('ABC').insertBefore(A, null);
+    pop.play();
 }
